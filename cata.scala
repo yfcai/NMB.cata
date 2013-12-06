edacbc3b08a7be929f4cf5334c0e783f696aabab
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-12-07 00:43:42 +0100

    attempt to use 2 functors together

--- cata.scala
+++ cata.scala
@@ -119,7 +119,6 @@ trait LambdaExpr extends AlgebraicDataType with Names {
       case _          => None
     }
 
-    private[LambdaExpr]
     case class Impl[T](val x: Name) extends Functor[T]
   }
 
@@ -136,7 +135,6 @@ trait LambdaExpr extends AlgebraicDataType with Names {
       case _          => None
     }
 
-    private[LambdaExpr]
     case class Impl[T](val fn: T, val arg: T) extends Functor[T]
   }
 
@@ -153,7 +151,6 @@ trait LambdaExpr extends AlgebraicDataType with Names {
       case _          => None
     }
 
-    private[LambdaExpr]
     case class Impl[T](val x: Name, val body: T) extends Functor[T]
   }
 
@@ -161,7 +158,16 @@ trait LambdaExpr extends AlgebraicDataType with Names {
   implicit def stringToADT(s: String): ADT = Var(s)
 }
 
-trait Calculus extends AlgebraicDataType with Names with Values {
+trait PrettyPrinter extends AlgebraicDataType {
+  def pretty(t: ADT): String = t fold prettyAlgebra
+
+  /** extension point: pretty print */
+  def prettyAlgebra: Algebra[String] = {
+    case x => sys error s"No clue how to pretty-print $x"
+  }
+}
+
+trait Calculus extends PrettyPrinter with Names with Values {
   type Env = PartialFunction[Name, Val]
 
   trait Evaluation {
@@ -177,13 +183,6 @@ trait Calculus extends AlgebraicDataType with Names with Values {
   /** extension point: evaluation operation */
   def evalAlgebra: Algebra[Env => Val] = {
     case x => sys error s"No clue how to evaluate $x"
-  }
-
-  def pretty(t: ADT): String = t fold prettyAlgebra
-
-  /** extension point: pretty print */
-  def prettyAlgebra: Algebra[String] = {
-    case x => sys error s"No clue how to pretty-print $x"
   }
 }
 
@@ -227,7 +226,6 @@ trait DebugExpr extends AlgebraicDataType {
   }
 
   object Debug {
-    private[DebugExpr]
     case class Impl[T](t: T) extends Functor[T]
     def apply(t: ADT): ADT = ADT(Impl(t))
     def unapply[T](e: Functor[T]): Option[T] = e match {
@@ -255,31 +253,191 @@ trait DebugCalculus extends Calculus with DebugExpr {
   }
 }
 
+class Test {
+  object TestCalculus extends LambdaCalculus with DebugCalculus
+  import TestCalculus._
 
-object TestCalculus extends LambdaCalculus with DebugCalculus
-import TestCalculus._
+  def test(t: => ADT) {
+    println(s"${pretty(t)} = ${eval(t)}")
+    println()
+  }
 
-def test(t: => ADT) {
-  println(s"${pretty(t)} = ${eval(t)}")
-  println()
+  test { App(App("+", Debug("2")), Debug("2")) }
+
+  test {
+    App(
+      App("fix", Abs("f", Abs("n",
+        App(App(App("if0", Debug("n")),
+          Abs("_", "0")),
+          Abs("_", App(App("+", "n"), App("f", App(App("-", "n"), "1")))))))),
+      "10")
+  }
+
+  test {
+    App(
+      App("fix", Abs("f", Abs("n",
+        App(App(App("if0", Debug("n")),
+          Abs("_", "1")),
+          Abs("_", App(App("*", "n"), App("f", App(App("-", "n"), "1")))))))),
+      "5")
+  }
 }
 
-test { App(App("+", Debug("2")), Debug("2")) }
+new Test
 
-test {
-  App(
-    App("fix", Abs("f", Abs("n",
-      App(App(App("if0", Debug("n")),
-        Abs("_", "0")),
-        Abs("_", App(App("+", "n"), App("f", App(App("-", "n"), "1")))))))),
-    "10")
+
+/** Experiment: fixed point of two functors.........
+  *
+  * type TypedTerm = Ft[Fu[Ft[Fu[...]]]]
+  */
+trait Typing {
+  object Term extends LambdaCalculus
+
+  type   Type = Type.ADT
+  object Type extends SimpleType
+
+  trait SimpleType extends AlgebraicDataType with PrettyPrinter {
+    override
+    def fmap[T, R](f: T => R): Functor[T] => Functor[R] = {
+      case IntType()              => IntType.Impl()
+      case FunType(domain, range) => FunType.Impl(f(domain), f(range))
+      case otherwise              => super.fmap(f)(otherwise)
+    }
+
+    override
+    def prettyAlgebra: Algebra[String] = {
+      case IntType()              => "ℤ"
+      case FunType(domain, range) => s"($domain → $range)"
+      case otherwise              => super.prettyAlgebra(otherwise)
+    }
+
+    object IntType {
+      def apply(): ADT = ADT(Impl())
+      def unapply[T](e: Functor[T]): Option[Unit] = e match {
+        case ADT(unroll) => unapplyUnrolled(unroll)
+        case _           => unapplyUnrolled(e)
+      }
+
+      private[this]
+      def unapplyUnrolled[T](e: Functor[T]): Option[Unit] = e match {
+        case i: Impl[T] if Impl.unapply(i) => Some(())
+        case _ => None
+      }
+
+      case class Impl[T]() extends Functor[T]
+    }
+
+    object FunType {
+      def apply(domain: ADT, range: ADT): ADT = ADT(Impl(domain, range))
+      def unapply[T](e: Functor[T]): Option[(T, T)] = e match {
+        case ADT(unroll) => unapplyUnrolled(unroll)
+        case _           => unapplyUnrolled(e)
+      }
+
+      private[this]
+      def unapplyUnrolled[T](e: Functor[T]): Option[(T, T)] = e match {
+        case i: Impl[T] => Impl.unapply(i)
+        case _          => None
+      }
+
+      case class Impl[T](domain: T, range: T) extends Functor[T]
+    }
+  }
+
+  object TypeAnnotation extends AlgebraicDataType {
+    override
+    def fmap[T, R](f: T => R): Functor[T] => Functor[R] = {
+      case NoNote(x) => NoNote.Impl(f(x))
+      case otherwise => super.fmap(f)(otherwise)
+    }
+
+    object NoNote {
+      case class Impl[T](t: T) extends Functor[T]
+
+      def apply(t: ADT): ADT = ADT(Impl(t))
+
+      def unapply[T](e: Functor[T]): Option[T] = e match {
+        case ADT(unroll) => unapplyUnrolled(unroll)
+        case _           => unapplyUnrolled(e)
+      }
+
+      private[this]
+      def unapplyUnrolled[T](e: Functor[T]): Option[T] = e match {
+        case i: Impl[T] => Impl.unapply(i)
+        case _          => None
+      }
+    }
+
+    object SomeNote {
+      case class Impl[T](note: Type, t: T) extends Functor[T]
+
+      def apply(note: Type, t: ADT): ADT = ADT(Impl(note, t))
+
+      def unapply[T](e: Functor[T]): Option[(Type, T)] = e match {
+        case ADT(unroll) => unapplyUnrolled(unroll)
+        case _           => unapplyUnrolled(e)
+      }
+
+      private[this]
+      def unapplyUnrolled[T](e: Functor[T]): Option[(Type, T)] = e match {
+        case i: Impl[T] => Impl.unapply(i)
+        case _          => None
+      }
+    }
+  }
+
+  // interlacing TypeAnnotation.Functor and Term.Functor
+  object TypedTerm extends AlgebraicDataType with PrettyPrinter {
+    override
+    def fmap[T, R](f: T => R): Functor[T] => Functor[R] = {
+      case TypedTerm(t) => TypedTerm(t map (_ map f)) // wow!
+      case otherwise    => super.fmap(f)(otherwise)
+    }
+
+    case class TypedTerm[T](
+      get: TypeAnnotation.Functor[Term.Functor[T]]
+    ) extends Functor[T]
+
+    override def prettyAlgebra: Algebra[String] = {
+      case TypedTerm(TypeAnnotation.SomeNote(tau, Term.Abs(x, body))) =>
+        "(λ$x : ${Type.pretty(tau)}. $body)"
+
+      case TypedTerm(TypeAnnotation.NoNote(app @ Term.App(_, _))) =>
+        Term.prettyAlgebra(app)
+
+      case TypedTerm(TypeAnnotation.NoNote(variable @ Term.Var(_))) =>
+        Term.prettyAlgebra(variable)
+
+      case otherwise =>
+        super.prettyAlgebra(otherwise)
+    }
+
+    implicit def typeNoteToTT(t: TypeAnnotation.Functor[Term.Functor[ADT]]):
+        ADT = ADT(TypedTerm(t))
+
+    implicit def untypedTermToTT(t: Term.Functor[ADT]): ADT =
+      TypeAnnotation.NoNote.Impl(t)
+  }
 }
 
-test {
-  App(
-    App("fix", Abs("f", Abs("n",
-      App(App(App("if0", Debug("n")),
-        Abs("_", "1")),
-        Abs("_", App(App("*", "n"), App("f", App(App("-", "n"), "1")))))))),
-    "5")
+class TestTyping extends Typing {
+  { // try out TypedTerm
+    import TypedTerm._
+    import Type.{IntType, FunType}
+    import TypeAnnotation.{SomeNote, NoNote}
+    import Term.{Abs, App, Var}
+
+    // o look at the type annotations
+    val t: ADT =
+      //NoNote.Impl[Term.Functor[ADT]](Var.Impl[ADT]("x"))
+
+      SomeNote.Impl[Term.Functor[ADT]](
+        IntType(),
+        Abs.Impl[ADT]("x", Var.Impl[ADT]("x"))
+      )
+
+      // interesting error here.
+      // TODO: copy build.sbt, wrap file in object, trace the stack
+  }
 }
+new TestTyping
