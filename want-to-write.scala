5d54b965c506fe4eb1cf4b73fbe9b91de74eabad
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-12-14 12:36:48 +0100

    make what we want to write easier to understand

--- want-to-write.scala
+++ want-to-write.scala
@@ -1,17 +1,17 @@
 trait WantToWrite {
   trait Term
 
-  // a case class with a binder as argument is always bound
-  case class Var(binder: Binder) extends Term
+  // a case class with a name as argument is always bound
+  case class Var(name: Name) extends Term
 
-  // a case class with a binder and a term as argument is
+  // a case class with a name and a term as argument is
   // always a binder
-  case class Abs(binder: Binder, body: Term) extends Term
+  case class Abs(name: Name, body: Term) extends Term
 
   case class App(operator: Term, operand: Term) extends Term
 
   case class Con(stant: String) extends Term
 
   // what we don't want to write, but is necessary to type check
-  type Binder
+  type Name
 }
