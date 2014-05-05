e3ffc1f06ab71b52e79da2498d8b3e86bee486ae
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-05-05 14:32:01 +0200

    make type rep work by converting implicit params to explicit ones

--- TypeRepresentations.scala
+++ TypeRepresentations.scala
@@ -1,57 +1,55 @@
-import language.implicitConversions
-
 trait TypeRepresentations {
   trait Rep[A]
 
-  implicit case object RInt extends Rep[Int]
+  case object RInt extends Rep[Int]
+  case object RUnit extends Rep[Unit]
 
   case class RProd[A, B](ra: Rep[A], rb: Rep[B]) extends Rep[(A, B)]
   case class RPlus[A, B](ra: Rep[A], rb: Rep[B]) extends Rep[Either[A, B]]
-  case class RView[A, B](iso: Iso[A, B], r: () => Rep[A]) extends Rep[B]
 
-  implicit def rProd[A, B](implicit ra: Rep[A], rb: Rep[B]): Rep[(A, B)] = RProd(ra, rb)
-  implicit def rPlus[A, B](implicit ra: Rep[A], rb: Rep[B]): Rep[Either[A, B]] = RPlus(ra, rb)
-  implicit def rView[A, B](implicit iso: Iso[A, B], r: () => Rep[A]): Rep[B] = RView(iso, r)
-
-  trait Iso[A, B] { def from: A => B ; def to: B => A }
-  case class KIso[A, B](from: A => B, to: B => A) extends Iso[A, B]
-
-  //implicit def commuteIso[A, B](iso: Iso[B, A]): Iso[A, B] = KIso(iso.to, iso.from)
+  trait Unroll[A, B] extends Rep[A] { def unroll: A => (B, Rep[B]) }
 
   type ListF[A, L] = Either[Unit, (A, L)]
 
-  def isoListF[A]: Iso[List[A], ListF[A, List[A]]] = KIso({
-    case Nil => Left(())
-    case x :: xs => Right((x, xs))
-  }, {
-    case Left(()) => Nil
-    case Right((x, xs)) => x :: xs
-  })
+  def repListF[A, L](ra: Rep[A], rl: Rep[L]): Rep[ListF[A, L]] = RPlus(RUnit, RProd(ra, rl))
 
-  // does not help... neither does commuteIso: Iso[A, B] => Iso[B, A].
-  implicit def isoListF2[A]: Iso[ListF[A, List[A]], List[A]] = {
-    val iso = isoListF[A]
-    KIso(iso.to, iso.from)
+  class RepList[A](ra: Rep[A]) extends Unroll[List[A], ListF[A, List[A]]] {
+    def unroll = {
+      case Nil => (Left(()), repListF(ra, this)) // tie the knot with `this`
+      case x :: xs => (Right((x, xs)), repListF(ra, this))
+    }
   }
 
-  implicit def thunkifyRep[A](r: => Rep[A]): () => Rep[A] = () => r
-
-  def sumAllInts[T](x: T)(implicit r: Rep[T]): Int = r match {
+  // strange thing:
+  // if pattern matching on `rep` isn't complete,
+  // then stack overflow instead of match error... why?
+  def sumAllInts[T](x: T)(rep: Rep[T]): Int = rep match {
+    case RUnit => 0 // if this is left out, then stack overflow
     case RInt => x
     case RProd(ra, rb) => sumAllInts(x._1)(ra) + sumAllInts(x._2)(rb)
     case RPlus(ra, rb) => x.fold(a => sumAllInts(a)(ra), b => sumAllInts(b)(rb))
-    case RView(iso, r) => sumAllInts(iso to x)(r())
+    case r: Unroll[_, _] => r unroll x match { case (b, rb) => sumAllInts(b)(rb) }
   }
+
+  // remaining questions
+  //
+  // 1. is there any way to have ADT carry their own type rep,
+  //    such that traversals need only take 1 type parameter?
+  //
+  // 2. every n-hole contexts of an ADT defines a functor.
+  //    every functor defines a map (commuting) function
+  //    is there any way to support all the map functions?
+  //    if there is, then we can also support replacing type
+  //    annotations in a term.
 }
 
-object TestTypeRepresentations extends TypeRepresentations { def main(args: Array[String]) {
-  val xs = (1 to 100).toList
-  // problem: diverging implicit expansion
-  val sumXs: Int =
-    // sumAllInts(xs) // could not find implicit value r: Rep[List[Int]]
-    // sumAllInts(xs)(rView) // could not find implicit value r: () => Rep[A]
-    // sumAllInts(xs)(rView(isoListF2, () => rPlus)) // diverging implicit expansion
-    ???
+object TestTypeRepresentations extends TypeRepresentations {
+  def main(args: Array[String]) {
 
-  // type rep may be useful in haskell. in scala, runtime instanceOf tests work better.
-}}
+    val xs = (1 to 100).toList
+
+    val sumXs: Int = sumAllInts(xs)(new RepList(RInt))
+
+    println(s"sum(1..100) = $sumXs")
+  }
+}
