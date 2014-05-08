39414e516ae45b7c6fcd6e807af0d3ebad71a556
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-05-08 20:00:25 +0200

    bad idea to specify functors by constructors. rectified.

--- NnaryFunctors.scala
+++ NnaryFunctors.scala
@@ -12,7 +12,7 @@ trait NnaryFunctors {
 
   // identity functor
   type ID[T] = T
-  object ID extends Functor[ID] {
+  implicit object ID extends Functor[ID] {
     def fmap[X, Y](f: X => Y) = f
   }
 
@@ -21,18 +21,15 @@ trait NnaryFunctors {
   case class Cons[+A, +B, +C](head: B, tail: C) extends ListF[A, B, C]
 
   sealed trait List[+A] extends ListF[Unit, A, List[A]] {
-    private[this] def functor3: Functor[ListF3[Unit, A, ID]#λ] =
-      Cons[Unit, A, Functor[ID]](null.asInstanceOf[A], ID)
-
     // can't create a trait for fixed points, because context bound Functor[F]
     // demands constructor argument and we can't have that.
-    def fold[T](f: ListF[Unit, A, T] => T): T = functor3.init(f)(this)
+    def fold[T](f: ListF[Unit, A, T] => T): T = listF3[Unit, A, ID].init(f)(this)
 
     def map[B](f: A => B): List[B] =
-      functor3.init[List[A], List[B]](
+      listF3[Unit, A, ID].init[List[A], List[B]](
         // you should really explore bifunctors a bit.
         // maybe they're all that's needed to make functors aware of all argument positions.
-        ys => Cons[Unit, Functor[ID], List[B]](ID, null.asInstanceOf[List[B]]).fmap(f)(ys)
+        ys => listF2[Unit, ID, List[B]].fmap(f)(ys)
       )(this)
   }
 
@@ -45,24 +42,24 @@ trait NnaryFunctors {
   type ListF2[A, F[_], C] = { type λ[B] = ListF[A, F[B], C] }
   type ListF3[A, B, F[_]] = { type λ[C] = ListF[A, B, F[C]] }
 
-  implicit class ListFunctor1[F[_], B, C](nil: Nil[Functor[F], B, C]) extends Functor[ListF1[F, B, C]#λ] {
+  def listFunctor1[F[_]: Functor, B, C]: Functor[ListF1[F, B, C]#λ] = new Functor[ListF1[F, B, C]#λ] {
     def fmap[X, Y](f: X => Y): ListF[F[X], B, C] => ListF[F[Y], B, C] = _ match {
-      case Nil(get) => new Nil(nil.get.fmap(f)(get))
+      case Nil(get) => new Nil(implicitly[Functor[F]].fmap(f)(get))
       case Cons(head, tail) => new Cons(head, tail)
     }
   }
 
-  implicit class ListFunctor2[A, F[_], C](cons: Cons[A, Functor[F], C]) extends Functor[ListF2[A, F, C]#λ] {
+  def listF2[A, F[_]: Functor, C]: Functor[ListF2[A, F, C]#λ] = new Functor[ListF2[A, F, C]#λ] {
     def fmap[X, Y](f: X => Y): ListF[A, F[X], C] => ListF[A, F[Y], C] = _ match {
       case Nil(get) => Nil(get)
-      case Cons(head, tail) => new Cons(cons.head.fmap(f)(head), tail)
+      case Cons(head, tail) => new Cons(implicitly[Functor[F]].fmap(f)(head), tail)
     }
   }
 
-  implicit class ListFunctor3[A, B, F[_]](cons: Cons[A, B, Functor[F]]) extends Functor[ListF3[A, B, F]#λ] {
+  def listF3[A, B, F[_]: Functor]: Functor[ListF3[A, B, F]#λ] = new Functor[ListF3[A, B, F]#λ] {
     def fmap[X, Y](f: X => Y): ListF[A, B, F[X]] => ListF[A, B, F[Y]] = _ match {
       case Nil(get) => new Nil(get)
-      case Cons(head, tail) => new Cons(head, cons.tail.fmap(f)(tail))
+      case Cons(head, tail) => new Cons(head, implicitly[Functor[F]].fmap(f)(tail))
     }
   }
 }
