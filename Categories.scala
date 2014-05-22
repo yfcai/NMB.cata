42a6d4dd90236315d906bdc1f2e994cb199cb993
Cai Yufei <cai@mathematik.uni-marburg.de>
2014-05-23 01:39:52 +0200

    overwrite bad categories with working ones

--- Categories.scala
+++ Categories.scala
@@ -1,89 +1,211 @@
+/** Formalizing category theory in scala does not seem practical.
+  * Nevertheless, you gained insight about natural transformations:
+  *
+  * A natural transformation is mapping against one argument of
+  * a curried multivariate functor.
+  */
+
 import language.higherKinds
-import language.implicitConversions
-import annotation.unchecked.uncheckedVariance
 
 trait Categories {
+  categories =>
+
+  trait Type {
+    type Type
+    def :*: (lhs: categories.Type) = new :*:(lhs, this)
+    def :+: (lhs: categories.Type) = new :+:(lhs, this)
+  }
+
   trait Category {
-    type Object
-    type Morphism[A <: Object, B <: Object]
-  }
+    type Object <: AnyRef
+    type Morphism <: ProtoMorphism
+    def id(x: Object): Morphism
 
-  class BaseC extends Category {
-    type Object = Any
-    type Morphism[A, B] = A => B
-  }
-
-  trait Functor[Domain <: Category, Range <: Category] {
-    type λ[X <: Domain#Object] <: Range#Object
-    def fmap[A <: Domain#Object, B <: Domain#Object](f: Domain#Morphism[A, B]): Range#Morphism[λ[A], λ[B]]
-  }
-
-  class IdF[C <: Category] extends Functor[C, C] {
-    type λ[X <: C#Object] = X
-    def fmap[A <: C#Object, B <: C#Object](f: C#Morphism[A, B]): C#Morphism[A, B] = f
-  }
-
-  // natural transformations (underconstrained; use with care.)
-  trait NaturalT[
-    Domain <: Category,
-    Range <: Category,
-    F <: Functor[Domain, Range],
-    G <: Functor[Domain, Range]
-  ] {
-    def eta[X <: Domain#Object]: Range#Morphism[F#λ[X], G#λ[X]]
-  }
-
-  // functor category
-  class FunctorC[Domain <: Category, Range <: Category] extends Category {
-    // objects are functors
-    type Object = Functor[Domain, Range]
-
-    // morphisms are natural transformations
-    type Morphism[F <: Object, G <: Object] = NaturalT[Domain, Range, F, G]
-  }
-
-  trait ProductType[A, B] {
-    // work around lack of variance reasoning
-    type _1 <: A
-    type _2 <: B
-    def _1: _1
-    def _2: _2
-  }
-
-  // product category
-  class ProductC[C1 <: Category, C2 <: Category] extends Category {
-    type Object = ProductType[C1#Object, C2#Object]
-
-    // can't put this in companion object coz it depends on type Object
-    trait Morphism[A <: Object, B <: Object] {
-      def _1: C1#Morphism[A#_1, B#_1]
-      def _2: C2#Morphism[A#_2, B#_2]
+    trait ProtoMorphism {
+      val domain: Object
+      val range: Object
     }
   }
 
-  // warm-up exercise: functor from cat to product with base
-  // pretty horrible to write.
-  class WithIntF[C <: Category] extends Functor[C, ProductC[BaseC, C]] {
-    type λ[X <: C#Object] = ProductType[Any, C#Object] {
-      type _1 = Int
-      type _2 = X
+  // constant categories are objects
+  object ScalaC extends Category {
+    type Object = Type
+    trait Morphism extends ProtoMorphism {
+      def apply(x: domain.Type): range.Type
+    }
+    def id(x: Object) = new Morphism {
+      val domain: x.type = x
+      val range: x.type = x
+      def apply(y: x.Type): x.Type = y
+    }
+  }
+
+  trait Functor {
+    val domain: Category
+    val range: Category
+    def map(x: domain.Object): range.Object
+    def fmap(f: domain.Morphism): range.Morphism
+  }
+
+  trait Endofunctor extends Functor {
+    val category: Category
+    val domain: category.type = category
+    val range : category.type = category
+  }
+
+  // concrete functors are case classes
+  case class IdF(c: Category) extends Functor {
+    import c.{Object, Morphism}
+    val domain: c.type = c
+    val range: c.type = c
+    def map(x: Object): Object = x
+    def fmap(f: Morphism): Morphism = f
+  }
+
+  // concrete categories are traits
+  trait FunctorC extends Category {
+    functorC =>
+    val c1: Category
+    val c2: Category
+    type Object = Functor
+    type Morphism = NaturalTransformation
+
+    def id(f: Object): Morphism = new NaturalTransformation {
+      val domain = f
+      val range  = f
+      def eta(x: c1.Object): c2.Morphism = c2.id(f map x)
     }
 
-    def fmap[A <: C#Object, B <: C#Object](f: C#Morphism[A, B]):
-        ProductC[BaseC, C]#Morphism[λ[A], λ[B]] = {
-      // must cast a null pointer; cannot say
-      //     new ProductC[BaseC, C]#Morphism[λ[A], λ[B]] { ... }
-      val c = null.asInstanceOf[ProductC[BaseC, C]]
-      new c.Morphism[λ[A], λ[B]] {
-        // _1 has to be identity function to preserve identity morphisms
-        def _1: Int => Int = identity[Int]
-        def _2: C#Morphism[A, B] = f
+    trait Functor extends categories.Functor {
+      val domain: c1.type
+      val range : c2.type
+    }
+
+    trait NaturalTransformation extends ProtoMorphism {
+      def eta(x: c1.Object): c2.Morphism
+    }
+  }
+
+  trait ProductC extends Category {
+    val c1: Category
+    val c2: Category
+
+    type Object = (c1.Object, c2.Object)
+
+    case class Morphism(
+      domain: Object, range: Object,
+      _1: c1.Morphism, _2: c2.Morphism
+    ) extends ProtoMorphism
+
+    def id(x: Object): Morphism = Morphism(x, x, c1.id(x._1), c2.id(x._2))
+  }
+
+  // functor from c1 to the functor category from c2 to the product category c1 × c2
+  case class ProductF(c1: Category, c2: Category) extends Functor {
+    productF =>
+
+    val product = new ProductC {
+      val c1: ProductF.this.c1.type = ProductF.this.c1
+      val c2: ProductF.this.c2.type = ProductF.this.c2
+    }
+
+    val domain: c1.type = c1
+    val range = new FunctorC {
+      val c1: ProductF.this.c2.type = ProductF.this.c2
+      val c2: ProductF.this.product.type = ProductF.this.product
+    }
+
+    def map(x: c1.Object): C2ToProduct = new C2ToProduct {
+      def map(y: c2.Object): product.Object = (x, y)
+      def fmap(g: c2.Morphism): product.Morphism =
+        product.Morphism((x, g.domain), (x, g.range), c1 id x, g)
+    }
+
+    def fmap(f: c1.Morphism): range.Morphism = new range.NaturalTransformation {
+      val domain: C2ToProduct = productF map f.domain
+      val range : C2ToProduct = productF map f.range
+      def eta(x: c2.Object): product.Morphism =
+        product.Morphism((f.domain, x), (f.range, x), f, c2 id x)
+    }
+
+    trait C2ToProduct extends range.Functor {
+      val domain: c2.type = c2
+      val range: product.type = product
+    }
+  }
+
+  def fix(f: Endofunctor): f.category.Object = f map fix(f)
+
+  object Unit extends Type {
+    type Type = Unit
+  }
+
+  case class :*:(_1: Type, _2: Type) extends Type {
+    type Type = (_1.Type, _2.Type)
+  }
+
+  case class :+:(left: Type, right: Type) extends Type {
+    type Type = Either[left.Type, right.Type]
+  }
+
+  trait ListPatternFunctor extends Functor {
+    listF =>
+    import ScalaC.Morphism
+
+    val domain = ScalaC
+    val range = new FunctorC {
+      val c1: ScalaC.type = ScalaC
+      val c2: ScalaC.type = ScalaC
+    }
+
+    def map(x: Type): ScalaEndofunctor = new ListFunctor2(x)
+
+    def fmap(f: Morphism): range.Morphism = new range.NaturalTransformation {
+      val domain = listF map f.domain
+
+      val range  = listF map f.range
+
+      def eta(x: Type): Morphism = new Morphism {
+        val domain = Unit :+: (f.domain :*: x)
+        val range  = Unit :+: (f.range  :*: x)
+        def apply(xs: domain.Type): range.Type = xs match {
+          case Left(_) => Left(().asInstanceOf[range.left.Type])
+          case Right((y, ys)) => Right((
+            f(y.asInstanceOf[f.domain.Type]),
+            ys)).asInstanceOf[range.Type]
+          case _ => sys error "OMG LOOK AT ALL THOSE CASTS"
+        }
       }
     }
-  }
-}
 
-object TestCategories extends Categories {
-  def main(args: Array[String]) {
+    trait ScalaEndofunctor extends range.Functor with Endofunctor {
+      import ScalaC.{Object, Morphism}
+      val category = ScalaC
+    }
+
+    class ListFunctor2(x: Type) extends ScalaEndofunctor {
+      def map(y: Type): Type = Unit :+: (x :*: y) // or even nominal
+      def fmap(f: Morphism): Morphism = new Morphism {
+        // exasperated sigh
+        val xfd = x :*: f.domain
+        val xfr = x :*: f.range
+        val domain = new :+: (Unit, xfd) {
+          override val left: Unit.type = Unit
+          override val right: xfd.type = xfd
+        }
+        val range  = new :+: (Unit, xfr) {
+          override val left: Unit.type = Unit
+          override val right: xfr.type = xfr
+        }
+        def apply(xs: domain.Type): range.Type = xs match {
+          case Left(()) => Left(())
+          case Right((head, tail)) => Right((
+            head.asInstanceOf[range.right._1.Type],
+            f(tail.asInstanceOf[f.domain.Type]).
+              asInstanceOf[range.right._2.Type]
+          ))
+        }
+      }
+    }
   }
 }
