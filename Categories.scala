a9c805fb13ed14257ea84a27cd6c99ff27b53923
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-05-22 14:42:31 +0200

    to experiment: categories as values
    Reason: need to obtain identity morphism

--- Categories.scala
+++ Categories.scala
@@ -6,11 +6,15 @@ trait Categories {
   trait Category {
     type Object
     type Morphism[A <: Object, B <: Object]
+
+    def id[X <: Object]: Morphism[X, X]
   }
 
   class BaseC extends Category {
     type Object = Any
     type Morphism[A, B] = A => B
+
+    def id[X]: X => X = identity
   }
 
   trait Functor[Domain <: Category, Range <: Category] {
@@ -23,7 +27,7 @@ trait Categories {
     def fmap[A <: C#Object, B <: C#Object](f: C#Morphism[A, B]): C#Morphism[A, B] = f
   }
 
-  // natural transformations (underconstrained; use with care.)
+  // natural transformations
   trait NaturalT[
     Domain <: Category,
     Range <: Category,
@@ -40,6 +44,8 @@ trait Categories {
 
     // morphisms are natural transformations
     type Morphism[F <: Object, G <: Object] = NaturalT[Domain, Range, F, G]
+
+    def id[F <: Object]: Morphism[F, F] = ???
   }
 
   trait ProductType[A, B] {
@@ -59,6 +65,8 @@ trait Categories {
       def _1: C1#Morphism[A#_1, B#_1]
       def _2: C2#Morphism[A#_2, B#_2]
     }
+
+    def id[X <: Object]: Morphism[X, X] = ???
   }
 
   // warm-up exercise: functor from cat to product with base
@@ -71,15 +79,37 @@ trait Categories {
 
     def fmap[A <: C#Object, B <: C#Object](f: C#Morphism[A, B]):
         ProductC[BaseC, C]#Morphism[λ[A], λ[B]] = {
-      // must cast a null pointer; cannot say
-      //     new ProductC[BaseC, C]#Morphism[λ[A], λ[B]] { ... }
-      val c = null.asInstanceOf[ProductC[BaseC, C]]
+      val c = new ProductC[BaseC, C]
       new c.Morphism[λ[A], λ[B]] {
         // _1 has to be identity function to preserve identity morphisms
         def _1: Int => Int = identity[Int]
         def _2: C#Morphism[A, B] = f
       }
     }
+  }
+
+  // functor for products
+  abstract class ProductF[C1 <: Category, C2 <: Category]
+      extends Functor[C1, FunctorC[C2, ProductC[C1, C2]]] {
+
+    type λ[X <: C1#Object] = Functor[C2, ProductC[C1, C2]] {
+      type λ[Y <: C2#Object] = ProductType[C1#Object, C2#Object] {
+        type _1 = X
+        type _2 = Y
+      }
+    }
+
+    def fmap[A <: C1#Object, B <: C1#Object](f: C1#Morphism[A, B]):
+        NaturalT[C2, ProductC[C1, C2], λ[A], λ[B]] =
+      new NaturalT[C2, ProductC[C1, C2], λ[A], λ[B]] {
+        def eta[X <: C2#Object]: ProductC[C1, C2]#Morphism[λ[A]#λ[X], λ[B]#λ[X]] = {
+          val c = new ProductC[C1, C2]
+          new c.Morphism[λ[A]#λ[X], λ[B]#λ[X]] {
+            def _1: C1#Morphism[A, B] = f
+            def _2: C2#Morphism[X, X] = ??? // identity morphism...
+          }
+        }
+      }
   }
 }
 
