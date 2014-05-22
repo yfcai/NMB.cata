11fa66af87c57da7b907dfd0fdb25de2f2fc7745
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-05-22 13:33:27 +0200

    add warm-up functor: X => (Int, X)

--- Categories.scala
+++ Categories.scala
@@ -8,7 +8,7 @@ trait Categories {
     type Morphism[A <: Object, B <: Object]
   }
 
-  object BaseCat extends Category {
+  class BaseC extends Category {
     type Object = Any
     type Morphism[A, B] = A => B
   }
@@ -18,13 +18,13 @@ trait Categories {
     def fmap[A <: Domain#Object, B <: Domain#Object](f: Domain#Morphism[A, B]): Range#Morphism[λ[A], λ[B]]
   }
 
-  class IdFun[C <: Category] extends Functor[C, C] {
+  class IdF[C <: Category] extends Functor[C, C] {
     type λ[X <: C#Object] = X
     def fmap[A <: C#Object, B <: C#Object](f: C#Morphism[A, B]): C#Morphism[A, B] = f
   }
 
   // natural transformations (underconstrained; use with care.)
-  trait NatT[
+  trait NaturalT[
     Domain <: Category,
     Range <: Category,
     F <: Functor[Domain, Range],
@@ -34,12 +34,52 @@ trait Categories {
   }
 
   // functor category
-  class FunCat[Domain <: Category, Range <: Category] extends Category {
+  class FunctorC[Domain <: Category, Range <: Category] extends Category {
     // objects are functors
     type Object = Functor[Domain, Range]
 
     // morphisms are natural transformations
-    type Morphism[F <: Object, G <: Object] = Nothing
+    type Morphism[F <: Object, G <: Object] = NaturalT[Domain, Range, F, G]
+  }
+
+  trait ProductType[A, B] {
+    // work around lack of variance reasoning
+    type _1 <: A
+    type _2 <: B
+    def _1: _1
+    def _2: _2
+  }
+
+  // product category
+  class ProductC[C1 <: Category, C2 <: Category] extends Category {
+    type Object = ProductType[C1#Object, C2#Object]
+
+    // can't put this in companion object coz it depends on type Object
+    trait Morphism[A <: Object, B <: Object] {
+      def _1: C1#Morphism[A#_1, B#_1]
+      def _2: C2#Morphism[A#_2, B#_2]
+    }
+  }
+
+  // warm-up exercise: functor from cat to product with base
+  // pretty horrible to write.
+  class WithIntF[C <: Category] extends Functor[C, ProductC[BaseC, C]] {
+    type λ[X <: C#Object] = ProductType[Any, C#Object] {
+      type _1 = Int
+      type _2 = X
+    }
+
+    def fmap[A <: C#Object, B <: C#Object](f: C#Morphism[A, B]):
+        ProductC[BaseC, C]#Morphism[λ[A], λ[B]] = {
+      // must cast a null pointer; cannot say
+      //     new ProductC[BaseC, C]#Morphism[λ[A], λ[B]] { ... }
+      val c = null.asInstanceOf[ProductC[BaseC, C]]
+      new c.Morphism[λ[A], λ[B]] {
+        // _1 has to be identity function to preserve identity morphisms
+        def _1: Int => Int = identity[Int]
+        def _2: C#Morphism[A, B] = f
+      }
+    }
   }
 }
 
