2105b41b7effafa3448705f4e6622d37b4c2b131
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-05-27 13:40:19 +0200

    try to tell scala that the fixed point of a bifunctor is a functor
    (#3)
    
    Worries:
    
    1. It seems manual unrolling is unavoidable.
       Please duplicate the development here with manual
       unrolling so that it's possible to write Mu.fmap.
    
    2. It's unclear whether we need more traits than Mu to
       handle more than 2 nested fixed point operators,
       or whether Mu alone is sufficient.
    
    3. How is this approach superior to generating fmap
       without regard for code reuse? With manual unrolling,
       this approach is hard to use because users have to
       remember to call unroll before *every* pattern matching
       and the compiler will not warn them if they forget.
       Maybe named fixed points + inferred type annotations
       are better?

--- FunctorRep.scala
+++ FunctorRep.scala
@@ -1,31 +1,26 @@
 import language.higherKinds
 
 trait FunctorRep {
-  trait Functor[F[_]] {
+  // functors are strictly positive
+  trait Functor[F[+_]] {
     def fmap[A, B]: (A => B) => F[A] => F[B]
-
-    // this method is impossible short of making every functor covariant
-    // which also makes functor instances untypeable
-    // sample error messsage: [+X](Nothing, X) expected, [X](Nothing, X) encountered
-    //
-    //def fix(x: G[Fix[G]]): Fix[G] = ???
   }
 
-  type Const[X] = { type λ[Y] = X }
+  type Const[X] = { type λ[+Y] = X }
 
-  type Identity[X] = X
+  type Identity[+X] = X
 
   case class P[+A, +B](_1: A, _2: B)
   sealed trait S[+A, +B]
   case class L[+A](get: A) extends S[A, Nothing]
   case class R[+B](get: B) extends S[Nothing, B]
 
-  type LP[F[_], G[_]] = {
-    type λ[X] = P[F[X], G[X]]
+  type LP[F[+_], G[+_]] = {
+    type λ[+X] = P[F[X], G[X]]
   }
 
-  type LS[F[_], G[_]] = {
-    type λ[X] = S[F[X], G[X]]
+  type LS[F[+_], G[+_]] = {
+    type λ[+X] = S[F[X], G[X]]
   }
 
   case class K[X]() extends Functor[Const[X]#λ] {
@@ -36,14 +31,14 @@ trait FunctorRep {
     def fmap[A, B] = identity
   }
 
-  case class :*:[F[_], G[_]](_1: Functor[F], _2: Functor[G])
+  case class :*:[F[+_], G[+_]](_1: Functor[F], _2: Functor[G])
       extends Functor[LP[F, G]#λ] {
     def fmap[A, B] = f => {
       case P(x, y) => P(_1.fmap(f)(x), _2.fmap(f)(y))
     }
   }
 
-  case class :+:[F[_], G[_]](_L: Functor[F], _R: Functor[G])
+  case class :+:[F[+_], G[+_]](_L: Functor[F], _R: Functor[G])
       extends Functor[LS[F, G]#λ] {
     def fmap[A, B] = f => {
       case L(x) => L(_L.fmap(f)(x))
@@ -54,9 +49,28 @@ trait FunctorRep {
   sealed trait Fix[+F[+_]] {
     this: F[Fix[F]] =>
     import annotation.unchecked.uncheckedVariance
-    // this.functor is write-only, do not call it elsewhere!
-    val functor: Functor[F] @uncheckedVariance
+    val functor: Functor[F] @uncheckedVariance // will it lead to soundness problems?
     def fold[T](f: F[T] => T): T = f(functor.fmap[Fix[F], T](_.fold(f))(this))
+  }
+
+  // fixed point of a bifunctor is a functor
+  // TODO: attempt more than 2 levels of recursion.
+  //       will it require another trait, or will Mu be fine?
+  trait Mu[H[+X, +R]] extends Functor[({ type λ[+X] = Fix[({ type λ[+R] = H[X, R] })#λ] })#λ] {
+    // ΛX. H[X, R] is a functor for all R.
+    def functor[R]: Functor[({ type λ[+X] = H[X, R] })#λ]
+
+    private[this] type Fixed[+X] = Fix[({ type λ[+R] = H[X, R] })#λ]
+
+    def fmap[A, B](f: A => B): Fixed[A] => Fixed[B] = fixedA => {
+      val fun = functor[Fixed[B]]
+      fixedA.fold[Fixed[B]](a_bs => {
+        val unrolled: H[B, Fixed[B]] = fun.fmap(f)(a_bs)
+        ??? : Fixed[B]
+        // the conversion from H[B, Fixed[B]] to Fixed[B] is impossible.
+        // TODO: consent to manual unrolling. there is no other way.
+      })
+    }
   }
 
   // CASE STUDY: LISTS
@@ -83,38 +97,47 @@ trait FunctorRep {
   // it will be Nil() from now on, never Nil.
   object Nil {
     def apply(): List[Nothing] =
+      // lack of manual unrolling is good,
+      // but it makes it rather hard to roll unknown things.
       new L(()) with Fix[List.F[Nothing]#λ] {
-        val functor = List.patternFunctor
+        val functor = List.patternFunctor[Nothing]
       }
 
-    /*
-    def unapply(xs: Either[_, _]): Boolean = xs match {
+    def unapply(xs: S[Unit, P[_, _]]): Boolean = xs match {
       // each case matches both the fixed point and the pattern...
-      case Left(()) => true
+      case L(()) => true
       case _ => false
-    } */
+    }
   }
-/*
+
   object Cons {
     def apply[A](x: A, xs: List[A]): List[A] =
-      ??? //Roll[List.PatternFunctor[A]#λ](Right((x, xs)))(List.patternFunctor)
+      new R(P(x, xs)) with Fix[List.F[A]#λ] {
+        val functor = List.patternFunctor[A]
+      }
 
-    def unapply[A](xs: Either[_, (A, List[A])]): Option[(A, List[A])] =
+    def unapply[A, L](xs: S[Unit, P[A, L]]): Option[(A, L)] =
       xs match {
-        case Right((x, xs)) => Some((x, xs))
+        case R(P(x, xs)) => Some((x, xs))
         case _ => None
       }
-  }*/
+  }
+
+  // CASE STUDY: EVEN/ODD
+
+  type EvenF[+O] = S[Unit, O]
+  type OddF[+E] = P[Unit, E]
 }
 
 object TestFunctorRep extends FunctorRep {
-/*
-  val xs: List[Int] = Nil()
+  val xs: List[Int] = Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))
 
   def main(args: Array[String]) {
-    // xs match { case Nil() => println("ok") } // uncaught MatchError
-    xs match {
-      case Nil() => println("ok")
+    val sum = xs.fold[Int] {
+      case Nil() => 0
+      case Cons(x, xs) => x + xs
     }
-  }*/
+    println(s"xs = $xs")
+    println(s"sum(xs) = $sum")
+  }
 }
